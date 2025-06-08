46ed7014710b5bed186f9df681af7b5a7cf8231f
Rachel Dooley <rae1821@gmail.com>
2025-06-07 20:15:19 -0400

    Changed hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import CTA from "@/components/CTA";
 // import Features from "@/components/Features";
 import Features2 from "@/components/Features2";
-import Hero from "@/components/Hero";
+// import Hero from "@/components/Hero";
+import { Hero1 } from "@/components/hero1";
 import HowItWorks from "@/components/HowItWorks";
 
 export default function Home() {
   return (
     <div className="mb-12">
-      <Hero />
+      <Hero1 />
 
       <div className="mt-4 px-8 mx-auto max-w-5xl">
         <HowItWorks />
---
+++ components/hero1.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import AnimatedTextUnderlign from "./AnimatedTextUnderlign";
+import { useRouter } from "next/navigation";
+
+const Hero1 = () => {
+  const router = useRouter();
+  return (
+    <section className="px-4 overflow-hidden py-32">
+      <div className="container flex flex-col items-center px-8 mx-auto">
+        <AnimatedTextUnderlign />
+        <p className="mb-8 max-w-xl text-gray-700 lg:text-lg text-center">
+          Some sort of cool awesome fancy descriptive text goes here.
+        </p>
+        <div className="mt-2 flex w-full flex-col justify-center gap-2 md:w-1/2 lg:w-[200px]">
+          <Button
+            className="group relative bg-transparent px-6 py-4 font-semibold text-black hover:cursor-pointer"
+            onClick={() => router.push("/login")}
+          >
+            <span className="absolute inset-0 size-full -translate-x-2 -translate-y-2 bg-red-300 transition duration-300 ease-out group-hover:translate-x-0 group-hover:translate-y-0"></span>
+            <span className="absolute inset-0 size-full border-4 border-black"></span>
+            <span className="relative">Get started</span>
+          </Button>
+        </div>
+      </div>
+      <div className="mt-8 flex flex-col items-center justify-center lg:mt-12">
+        <div className="b relative mx-auto aspect-square w-[95%] max-w-[31.25rem] sm:w-full">
+          <div className="absolute inset-0 z-5 m-auto flex aspect-29/36 w-4/5 max-w-[16rem] translate-x-[-75%] translate-y-[10%] scale-[0.85] rotate-[-15deg] justify-center rounded-lg border border-border bg-accent opacity-60 md:w-[21.25rem] md:max-w-[21.25rem]">
+            <Image
+              src="/images/hero.jpg"
+              alt="hero section demo image showing interface components"
+              width={400}
+              height={400}
+              loading="lazy"
+              className="w-full rounded-md object-cover"
+            />
+          </div>
+          <div className="absolute inset-0 z-10 m-auto flex aspect-29/36 w-4/5 max-w-[16rem] justify-center rounded-lg border border-border bg-accent md:w-[21.25rem] md:max-w-[21.25rem]">
+            <Image
+              src="/images/hero.jpg"
+              alt="hero section demo image showing interface components"
+              width={400}
+              height={400}
+              loading="lazy"
+              className="w-full rounded-md object-cover"
+            />
+          </div>
+          <div className="absolute inset-0 z-5 m-auto flex aspect-29/36 w-4/5 max-w-[16rem] translate-x-[75%] translate-y-[10%] scale-[0.85] rotate-[15deg] justify-center rounded-lg border border-border bg-accent opacity-60 md:w-[21.25rem] md:max-w-[21.25rem]">
+            <Image
+              src="/images/hero.jpg"
+              alt="hero section demo image showing interface components"
+              width={400}
+              height={400}
+              loading="lazy"
+              className="w-full rounded-md object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export { Hero1 };
